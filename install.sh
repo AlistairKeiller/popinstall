a0346773d4fb3619612cc16e886c125f3622839c
Alistair Keiller <alistair@keiller.net>
2022-11-21 14:46:29 -0800

    add custom keybindings

--- install.sh
+++ install.sh
@@ -22,6 +22,22 @@ chsh -s /usr/bin/fish
 git config --global user.name 'Alistair Keiller'
 git config --global user.email 'alistair@keiller.net'
 
+# set keyboard shortcuts
+# set vscode to super+e
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ binding '<Super>e'
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ command 'code'
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ name 'Launch vscode'
+gsettings set org.gnome.settings-daemon.plugins.media-keys email '@as []'
+# set discord to super+d
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/ binding '<Super>d'
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/ command 'flatpak run com.discordapp.Discord'
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/ name 'Launch discord'
+gsettings set org.gnome.shell.keybindings toggle-overview '@as []'
+
+gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/PopLaunch1/', '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/', '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/']"
+
 # configure sound
 
 # configure displays
+
+# add flatpak run com.discordapp.Discord --start-minimized on startup
