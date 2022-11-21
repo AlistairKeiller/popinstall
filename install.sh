a7dcb7c434c0b6dbaa6d2a338928b73f91fb9be5
Alistair Keiller <alistair@keiller.net>
2022-11-21 13:55:15 -0800

    add julia and rust

--- install.sh
+++ install.sh
@@ -17,7 +17,7 @@ flatpak install flathub com.discordapp.Discord
 flatpak install flathub com.skype.Client
 flatpak install flathub com.microsoft.Teams
 flatpak install flathub us.zoom.Zoom
-sudo apt install -y code fish fonts-jetbrains-mono
+sudo apt install -y code fish fonts-jetbrains-mono julia rust-all
 chsh -s /usr/bin/fish
 git config --global user.name 'Alistair Keiller'
 git config --global user.email 'alistair@keiller.net'
